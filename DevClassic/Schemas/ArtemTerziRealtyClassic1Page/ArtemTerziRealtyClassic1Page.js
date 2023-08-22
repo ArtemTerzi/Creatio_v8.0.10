70e0093df993d1f39f8565b66f3e46a1f57882f5
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-08-22 21:43:42 +0300

    ADDED: ClassicUIRealtyPagy + RealtyClassicUI Objs

--- DevClassic/Schemas/ArtemTerziRealtyClassic1Page/ArtemTerziRealtyClassic1Page.js
+++ DevClassic/Schemas/ArtemTerziRealtyClassic1Page/ArtemTerziRealtyClassic1Page.js
@@ -1,7 +1,22 @@
 define("ArtemTerziRealtyClassic1Page", [], function() {
 	return {
 		entitySchemaName: "ArtemTerziRealtyClassic",
-		attributes: {},
+		attributes: {
+			"ArtemTerziCommissionUSD": {
+				dependencies: [
+					{
+						columns: ["ArtemTerziPriceUSD", "ArtemTerziOfferType"],
+						methodName: "calculateCommission"
+					}
+				],
+				"caption": "Commission, USD"
+			},
+			"ArtemTerziOfferType": {
+				lookupListConfig: {
+					columns: ["ArtemTerziCommissionPercent"]
+				}
+			}
+		},
 		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
 		details: /**SCHEMA_DETAILS*/{
 			"Files": {
@@ -14,7 +29,42 @@ define("ArtemTerziRealtyClassic1Page", [], function() {
 			}
 		}/**SCHEMA_DETAILS*/,
 		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
-		methods: {},
+		methods: {
+			calculateCommission: function() {
+				var price = this.get("ArtemTerziPriceUSD");
+				if (!price) {
+					price = 0;
+				}
+				var offerTypeObject = this.get("ArtemTerziOfferType");
+				var percent = 0;
+				if (offerTypeObject) {
+					percent = offerTypeObject.ArtemTerziCommissionPercent;
+				}
+				var commission = price * percent / 100;
+				this.set("ArtemTerziCommissionUSD", commission);
+			},
+			
+			onEntityInitialized: function() {
+				this.callParent(arguments);
+				this.calculateCommission();
+			},
+			
+			positiveValueValidator: function(value, column) {
+				var msg = "";
+				if (value < 0) {
+					msg = this.get("Resources.Strings.ValueMustBeGreaterThanZero");
+				}
+				return {
+					invalidMessage: msg
+				};
+			},
+
+			setValidationConfig: function() {
+				this.callParent(arguments);
+				this.addColumnValidator("ArtemTerziPriceUSD", this.positiveValueValidator);
+				this.addColumnValidator("ArtemTerziArea", this.positiveValueValidator);
+			},
+		},
 		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
 		diff: /**SCHEMA_DIFF*/[
 			{
@@ -34,6 +84,141 @@ define("ArtemTerziRealtyClassic1Page", [], function() {
 				"parentName": "ProfileContainer",
 				"propertyName": "items",
 				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "FLOATbe20d8da-251f-457a-ad12-14b4c7c32615",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "ArtemTerziPriceUSD",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "FLOAT5dbbabfc-103b-46cb-bf34-ef724cf56408",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 2,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "ArtemTerziArea",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "FLOAT1fa46e97-742b-4065-9e14-406a1418b644",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 3,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "ArtemTerziCommissionUSD",
+					"enabled": false
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUPe3166c4b-faa3-4b50-a57c-2f39e9d7ad75",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "ArtemTerziType",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUP0b56e73d-5175-4062-9f75-a6a0337e177b",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "ArtemTerziOfferType",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "STRING17b5dbbf-c63f-442e-8c47-acda649e2c6c",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "ArtemTerziComment",
+					"tip": {
+						"content": {
+							"bindTo": "Resources.Strings.STRING17b5dbbfc63f442e8c47acda649e2c6cTip"
+						}
+					},
+					"enabled": true,
+					"contentType": 0
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUPaa266c83-8739-45da-a539-3d65d6adc043",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "ArtemTerziManager",
+					"enabled": true,
+					"contentType": 5
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 3
 			},
 			{
 				"operation": "insert",
